feb43cdeae7e9dce2d2331bc666bcd93cf1e6dde
colbyguan <colbyguan@gmail.com>
2015-09-23 22:41:46 -0700

    Fancy Yogi Berra keyboard done

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -11,6 +11,25 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var yogiButton: UIButton!
+    @IBOutlet var newlineButton: UIButton!
+    @IBOutlet var backspaceButton: UIButton!
+    let yogiisms = [
+        "When you come to a fork in the road, take it.",
+        "You can observe a lot by just watching.",
+        "It ain't over till it's over",
+        "It's like déjà vu all over again.",
+        "No one goes there nowadays, it’s too crowded.",
+        "Baseball is ninety percent mental and the other half is physical.",
+        "A nickel ain't worth a dime anymore.",
+        "Always go to other people's funerals, otherwise they won't come to yours.",
+        "We made too many wrong mistakes.",
+        "Congratulations. I knew the record would stand until it was broken.",
+        "I want to thank everyone for making this night necessary.",
+        "Take it with a grin of salt.",
+        "It gets late early out here.",
+        "The future ain’t what it used to be.",
+        "He hits from both sides of the plate. He’s amphibious."]
     
     var keyboardView: UIView!
 
@@ -23,6 +42,7 @@ class KeyboardViewController: UIInputViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadInterface()
+        loadTargets()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +65,26 @@ class KeyboardViewController: UIInputViewController {
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+    }
+    
+    func loadTargets() {
+        yogiButton.addTarget(self, action: "yogi", forControlEvents: .TouchUpInside)
+        newlineButton.addTarget(self, action: "newline", forControlEvents: .TouchUpInside)
+        backspaceButton.addTarget(self, action: "backspace", forControlEvents: .TouchUpInside)
+    }
+    
+    func yogi() {
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        let idx = Int(arc4random_uniform(UInt32(yogiisms.count)))
+        proxy.insertText(yogiisms[idx])
+    }
+    func newline() {
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("\n")
+    }
+    func backspace() {
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.deleteBackward()
     }
 
 
